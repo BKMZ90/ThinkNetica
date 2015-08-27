53b4c3dc47682e9717cd62dfcd1f47c975ea44fd
BKMZ90 <BKMZ90@list.ru>
2015-08-28 00:57:14 +0500

    fulfilled the task 3 and without complex task 4

--- Dispetcher/train.rb
+++ Dispetcher/train.rb
@@ -1,43 +1,47 @@
 class Train
-	
+	attr_reader :speed
+
 	def initialize(type, wagon)
 		@type = type
 		@wagon = wagon
-		@speed = BEGIN_SPEED
+		@speed = begin_speed
 		@route = []
 	end
 
-	def speed_up(speed)													#поезд набирает скорость
-		@speed += speed
+	def speed_up(speed_up)													#поезд набирает скорость
+		@speed += speed_up
 	end
 
-	def speed_down(speed)												#поезд тормозит
-		@speed -= speed if @speed > BEGIN_SPEED
+	def speed_down(speed_up)												#поезд тормозит
+		@speed -= speed_up if @speed > begin_speed
+	else
+		@speed = begin_speed   #чтобы поезд не имел обратную скорость
 	end
 
 	def stop																		#остановка поезда
-		self.speed = BEGIN_SPEED
+		self.speed = begin_speed
 	end
 
-	def Current_speed														#текущая скорость поезда
-		Puts "#{@speed}"
+	def current_speed														#текущая скорость поезда
+		puts "#{@speed}"
 	end
 
-	def train_length														#показываем длину поезда
+	def length														#показываем длину поезда
 		puts "Train length = #{@wagon}"
 	end
 
-	def add_wagon																#прицепляем вагоны согласно условию (по одному вагону за операцию, метод просто увеличивает количество вагонов). 
-		 if @speed == BEGIN_SPEED									#Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
-			@wagon += 1 
-			else
-				puts "Warning: The train in motion"
+	def add_wagon							#прицепляем вагоны согласно условию (по одному вагону за операцию, метод просто увеличивает количество вагонов). 
+		 									#Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
+		if check_speed?
+			add_wagon! 
+		else
+			puts "Warning: The train in motion"
 		end
 	end
 
 	def remote_wagon											#отцепляем вагоны согласно условию (по одному вагону за операцию, метод просто увеличивает количество вагонов). 
-		if @speed == BEGIN_SPEED
-			@wagon -= 1
+		if check_speed?
+			remote_wagon!
 			else
 				puts "Warning: The train in motion"
 		end
@@ -51,17 +55,34 @@ class Train
 		@route = []
 	end
 
-	protected
+protected
 
 	attr_reader :type														#тип вагона нам потребуется пожалуй только для чтения
-	attr_accessor :speed
+	attr_writer :speed
 	attr_accessor :route
+
+	def check_speed?
+		@speed.zero? # = begin_speed
+	end
+
+	def add_wagon!
+		@wagon += 1
+	end
+	
+	def remote_wagon!
+		@wagon -= 1
+	end
+	
+	def begin_speed      #начальная скорость, однако работает и без этого метода
+		0
+	end
+
 
 	#def check_speed 			#закрываем проверку скорости от посторонних глаз
 	#	@speed = BEGIN_SPEED
 	#end
 
-	BEGIN_SPEED = 0
+	#BEGIN_SPEED = 0
 end
 
 
